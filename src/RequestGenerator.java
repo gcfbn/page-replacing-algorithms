89241e0f7ea6350d11edc63429bf8bcc0bcb8600
gcfbn <bmekarski@interia.pl>
2021-04-22 20:57:22 +0200

    Added new parameter (maxRequestDistance) to RequestGenerator.

--- src/RequestGenerator.java
+++ src/RequestGenerator.java
@@ -3,7 +3,8 @@ import java.util.Random;
 
 public class RequestGenerator {
 
-    public static ArrayList<Integer> generateRequest(int length, int memorySize){
+
+    public static ArrayList<Integer> generateRequest(int length, int memorySize, int maxRequestDistance){
 
         Random randomGenerator = new Random();
         int startIndex = randomGenerator.nextInt(memorySize);
@@ -14,14 +15,17 @@ public class RequestGenerator {
         int previousRequest = startIndex;
 
         for (int i=1; i<length; i++){
-            // next requested page will have index in range [previousRequest - 10, previousRequest + 10]
-            int nextRequest = previousRequest + randomGenerator.nextInt(21) - 10;
+            // next requested page will have index in range
+            // [previousRequest - maxRequestDistance, previousRequest + maxRequestDistance]
+            int nextRequest = previousRequest + randomGenerator.nextInt(2 * maxRequestDistance + 1) - maxRequestDistance;
 
-            // if index is out of bounds (index >= memorySize), set it to [memorySize - 10, memorySize - 1]
-            if (nextRequest >= memorySize) nextRequest = memorySize - randomGenerator.nextInt(10) + 1;
+            // if index is out of bounds (index >= memorySize),
+            // set it to [memorySize - maxRequestDistance, memorySize - 1]
+            if (nextRequest >= memorySize)
+                nextRequest = memorySize - (randomGenerator.nextInt(maxRequestDistance) + 1);
 
-            // if index is out of bounds (index < 0), set it to [0,9]
-            else if (nextRequest < 0) nextRequest = randomGenerator.nextInt(10);
+            // if index is out of bounds (index < 0), set it to [0,4]
+            else if (nextRequest < 0) nextRequest = randomGenerator.nextInt(maxRequestDistance);
 
             requests.add(nextRequest);
             previousRequest = nextRequest;
